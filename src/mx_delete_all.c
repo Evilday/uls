f31b8b9c1bc521cf4da4b588d606e8ec0e3e778b
vkmetyk <wova320@gmail.com>
2019-11-28 18:21:11 +0200

    del

--- src/mx_delete_all.c
+++
@@ -1,7 +0,0 @@
-#include "uls.h"
-
-void mx_del_info(t_info **info) {
-	if (*info) {
-		
-	}
-}
--- src/mx_work_with_flags.c
+++
@@ -1,5 +0,0 @@
-#include "uls.h"
-
-void mx_work_with_flags(t_info *info) {
-	printf("FLAGS exist\n");
-}
