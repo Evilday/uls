8d62f5638160b4696baf2604d82901b5fc79ebca
vkmetyk <wova320@gmail.com>
2019-11-30 19:37:15 +0200

    v_003

--- inc/uls.h
+++ inc/uls.h
@@ -10,15 +10,19 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <sys/stat.h>
-
-#include <wchar.h>
 #include <sys/ioctl.h>
 #include <sys/acl.h>
 #include <sys/types.h>
-#include <pwd.h>
-#include <grp.h>
 #include <sys/acl.h>
 #include <sys/xattr.h>
+#include <wchar.h>
+#include <pwd.h>
+#include <grp.h>
+
+typedef struct s_uni_list {
+	char *data;
+	struct s_uni_list *next;
+} t_uni_list;
 
 typedef struct s_info {
 	int argc;
@@ -27,11 +31,16 @@ typedef struct s_info {
 	bool flags_exist;
 	bool args_exist;
 
-	char *flags;
-	bool *exist;
+	int *where_what; // 0 - не валідна, 1 - флажок, 2 - агрумент
+
+	char *all_our_flags; // список всіх наших флагів, які нам прийшли
+	
+	struct s_uni_list *sub_args; // назви файлів, які є в аргументі
+	struct s_uni_list *access; // доступ до файлів
 } t_info;
 
 // mx_check_errors
+char *mx_up_to_one(char *str);
 bool mx_check_flags(t_info *info, int i);
 bool mx_check_argv(t_info *info, int i);
 
@@ -51,5 +60,15 @@ void mx_sort_args(t_info *info);
 
 // mx_work_with_flags
 void mx_work_with_flags(t_info *info);
+void mx_l_flag(t_info *info);
+void mx_take_flags(t_info *info);
+
+// mx_window.c
+int mx_num_of_cols(t_info *info);
+
+// mx_list
+t_uni_list *mx_create_uni_list(char *data);
+void mx_push_uni_list_back(t_uni_list **list, void *data);
+void mx_pop_uni_list_front(t_uni_list **head);
 
 #endif
--- src/mx_work_with_args.c
+++ src/mx_work_with_args.c
@@ -7,11 +7,11 @@ void mx_sort_args(t_info *info) {
 		for (int j = 0; info->argv[j + 1 + i]; j++) {
 			if (mx_strcmp(info->argv[j], info->argv[j + 1]) > 0) {
 				temp_str = info->argv[j];
-				temp_id = info->exist[j];
+				temp_id = info->where_what[j];
 				info->argv[j] = info->argv[j + 1];
-				info->exist[j] = info->exist[j + 1];
+				info->where_what[j] = info->where_what[j + 1];
 				info->argv[j + 1] = temp_str;
-				info->exist[j + 1] = temp_id;
+				info->where_what[j + 1] = temp_id;
 			}
 		}
 	}
@@ -33,6 +33,7 @@ static void default_args(t_info *info) {
 		for (int i = 0; i < num_of_elems; i++) {
 			d = readdir(f);
 			all_elems[i] = mx_strdup(d->d_name);
+			where_what[i] = 2;
 		}
 	}
 	closedir(f);
@@ -40,9 +41,84 @@ static void default_args(t_info *info) {
 	info->argv = all_elems;
 }
 
+static bool else_look_sub_argv(t_info *info, char *file, int i) {
+	DIR *f;
+	struct dirent *d;
+
+	if ((f = opendir(file))) {
+		while((d = readdir(f)))
+			mx_push_uni_list_back(&(info->sub_args), d->d_name);
+			closedir(f);
+			return 1;
+	}
+	else {
+		mx_push_uni_list_back(&(info->sub_args), info->argv[i]);
+		return 1;
+	}
+	return 0;
+}
+
+bool mx_look_sub_argv(t_info *info, int i) {
+	DIR *f;
+	struct dirent *d;
+	char *file;
+
+	if ((f = opendir(info->argv[i]))) {
+		while ((d = readdir(f))) {
+			mx_push_uni_list_back(&(info->sub_args), d->d_name);
+		}
+		closedir(f);
+		return 1;
+	}
+	else {
+		file = mx_up_to_one(info->argv[i]);
+		if (else_look_sub_argv(info, file, i)) {
+			free(file);
+			return 1;
+		}
+		free(file);
+	}
+	return 0;
+}
+
+void mx_print_arg(t_info *info) {
+	for (t_uni_list *tmp = info->sub_args; tmp; tmp = tmp->next) {
+		mx_printstr(tmp->data);
+		mx_printchar('\n');
+	}
+	mx_printchar('\n');
+}
+
+void mx_file_or_folder() {
+	
+}
+
+void mx_work_with_one_arg(t_info *info, int i) {
+	mx_look_sub_argv(info, i);
+	if (info->flags_exist)
+		mx_work_with_flags(info);
+	mx_print_arg(info);
+	// for (t_uni_list *tmp = info->access; tmp; tmp = tmp->next)
+	// 	printf("ARG: %s\n", tmp->data);
+	// for (t_uni_list *tmp = info->sub_args; tmp; tmp = tmp->next) {
+	// 	mx_pop_uni_list_front(&(info->sub_args));
+	// }
+	// for (t_uni_list *tmp = info->access; tmp; tmp = tmp->next) {
+	// 	mx_pop_uni_list_front(&(info->access));
+	// }
+	printf("\n");
+}
+
 void mx_work_with_args(t_info *info) {
+	if (info->flags_exist)
+		mx_take_flags(info);
 	if (info->args_exist)
-		printf("ARGS exist\n");
+		printf("Args exist !\n");
 	else
 		default_args(info);
+	for (int i = 0; i < info->argc; i++) {
+		if (info->where_what[i] == 2) {
+			mx_work_with_one_arg(info, i);
+		}
+	}
 }
